1bf37924f1e851d573a994cd5dc03e8c8e1352f1
jikur <jikur3667@naver.com>
2018-04-02 01:56:20 +0900

    #10

---
+++ app/src/main/java/org/androidtown/newjjjproject/FirstFragment.java
@@ -0,0 +1,25 @@
+package org.androidtown.newjjjproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends android.support.v4.app.Fragment {
+
+
+    public FirstFragment()
+    {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //activity_first_fragment 를 인플레이트해 (엑티비티가 아님)
+        View view = inflater.inflate(R.layout.activity_first_fragment, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/org/androidtown/newjjjproject/HomeActivity.java
@@ -0,0 +1,50 @@
+package org.androidtown.newjjjproject;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        initView();
+        setAdapter();
+        setListener();
+    }
+
+    public void initView()
+    {
+        viewPager = findViewById(R.id.viewPager_home);
+        tabLayout = findViewById(R.id.tabLayout);
+    }
+
+    public void setAdapter()
+    {
+        CustomAdapter customAdapter = new CustomAdapter(getSupportFragmentManager()); //fragment 에 접근
+        viewPager.setAdapter(customAdapter);
+    }
+
+
+    public void setListener()
+    {
+        //탭 버튼을 눌렀을 때 뷰 페이저 바꾸기
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+
+    public void start()
+    {
+        //Intent intent = new Intent(this, );
+        //startActivity(intent);
+    }
+
+
+}
